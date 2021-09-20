b7409498452902fa0aa22dee12142144b44e4277
pravallikaaaa <pravapas@LIN65003981.corp.capgemini.com>
2021-09-20 11:35:42 +0530

    sum added in show method

--- sample1/src/main/java/sample1/boot.java
+++ sample1/src/main/java/sample1/boot.java
@@ -7,6 +7,12 @@ show();
 }
 	
 	public static void show() {
+		int[] arr= {1,2,3,4,5};
+		int sum=0;
+		for(int n:arr)
+		{  sum+=n;
+			System.out.println(n);
+		}
 		System.out.println("hello 2");
 	}
 }
